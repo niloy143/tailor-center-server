18f2e68f30d6f943309f4eb377dc7103954c0642
niloy143 <niloymahmudapu@gmail.com>
2022-11-09 02:01:30 +0600

    connected to mongodb

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
@@ -8,6 +9,27 @@ const jwtAccessCode = process.env.JWT_ACCESS_CODE;
 
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.sq5icdb.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        const servicesCollection = client.db('tailorCenter').collection('services');
+
+        app.get('/services', async (req, res) => {
+            const limitation = parseInt(req.query.count) || 0;
+            const cursor = servicesCollection.find({});
+            const services = await cursor.limit(limitation).toArray();
+            res.send(services);
+        })
+    }
+    catch (err) {
+        console.log(err.code)
+    }
+}
+
+run().catch(err => console.log(err))
 
 app.get('/', (req, res) => {
     res.send({ status: 'running' })
