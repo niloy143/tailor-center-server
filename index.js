003751595c3dd4cb13e85b8aa08ce955ee49f96d
niloy143 <niloymahmudapu@gmail.com>
2022-11-08 15:04:47 +0600

    jwt created and stored

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const app = express();
 const port = process.env.PORT || 5000;
+const jwtAccessCode = process.env.JWT_ACCESS_CODE;
 
 app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.send({status: 'running'})
+    res.send({ status: 'running' })
+})
+
+app.post('/jwt', (req, res) => {
+    const user = req.body;
+    const token = jwt.sign(user, jwtAccessCode);
+    res.send({ token });
 })
 
 app.listen(port, () => {
